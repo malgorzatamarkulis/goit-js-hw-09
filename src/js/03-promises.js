d5ad836e5428f6503c031362e6b205462bf216c6
MalgorzataM <malgorzata.markulis@gmail.com>
2023-12-12 15:10:16 +0100

    03 promises done

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,32 @@
+import Notiflix from 'notiflix';
+
+let amount;
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+        resolve();
+
+        // Fulfill
+      } else {
+        return Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+        reject({ position, delay });
+        // Reject
+      }
+    }, delay);
+  });
+};
+document.querySelector("form").addEventListener("submit", function (event) {
+  event.preventDefault();
+  const amount = parseInt(document.querySelector("[name = 'amount']").value);
+  const delay = parseInt(document.querySelector("[name = 'delay']").value);
+  const step = parseInt(document.querySelector("[name = 'step']").value);
+
+  let currentDelay = delay;
+  for (let i = 1; i <= amount; i++) {
+    createPromise(i, currentDelay);
+    currentDelay += step;
   }
-}
+});
