6bf0d727fa3f564010d674be8cc342c23f92d581
MalgorzataM <malgorzata.markulis@gmail.com>
2023-12-13 16:58:57 +0100

    03-promises fix

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,32 +1,38 @@
 import Notiflix from 'notiflix';
 
-let amount;
 function createPromise(position, delay) {
   return new Promise((resolve, reject) => {
     const shouldResolve = Math.random() > 0.3;
+
     setTimeout(() => {
       if (shouldResolve) {
         Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
-        resolve();
-
-        // Fulfill
+        resolve({ position, delay });
       } else {
-        return Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+        Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
         reject({ position, delay });
-        // Reject
       }
     }, delay);
   });
-};
+}
+
 document.querySelector("form").addEventListener("submit", function (event) {
   event.preventDefault();
-  const amount = parseInt(document.querySelector("[name = 'amount']").value);
-  const delay = parseInt(document.querySelector("[name = 'delay']").value);
-  const step = parseInt(document.querySelector("[name = 'step']").value);
+  const amount = parseInt(document.querySelector("[name='amount']").value);
+  const delay = parseInt(document.querySelector("[name='delay']").value);
+  const step = parseInt(document.querySelector("[name='step']").value);
 
   let currentDelay = delay;
+
   for (let i = 1; i <= amount; i++) {
-    createPromise(i, currentDelay);
+    createPromise(i, currentDelay)
+      .then(({ position, delay }) => {
+        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+
     currentDelay += step;
   }
 });
